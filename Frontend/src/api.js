32e9e295ac6eeda8d69ec67c6b1813e3ac50f9cc
AnkitDhakad4 <231220011@nitdelhi.ac.in>
2026-01-19 18:40:28 +0530

    Change API URL to local server for development
    Updated API endpoint to use local development server.

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const fetchProjectsBySkill = async (skill) => {
   try {
     const res = await axios.get(
-      `https://predusk-ka-assingment-dywx.vercel.app/projects?skill=${skill}`
+      `http://localhost:3000/api/v1/projects?skill=${skill}`
     );
   
     // console.log("Response from the api is",res.data.data)
